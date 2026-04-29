96a67a818ece006a66d67a86d930c33d98d24c32
hai68pham <phhai682006mt@gmail.com>
2026-04-29 10:42:42 +0700

    Refactor DES code and add fake encryption methods
    Refactor DES implementation with simplified functions and added fake encryption/decryption methods.

--- des.cpp
+++ des.cpp
@@ -1,247 +1,91 @@
-#include <iostream>
-#include <string>
-#include <bitset>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-// ================= HELPER =================
-string xor_str(const string &a, const string &b) {
+// XOR 2 chuỗi nhị phân
+string xor_str(string a, string b) {
     string res = "";
-    for (int i = 0; i < a.size(); i++)
-        res += (a[i] != b[i]) ? '1' : '0';
+    for (int i = 0; i < a.size(); i++) {
+        res += (a[i] == b[i]) ? '0' : '1';
+    }
     return res;
 }
 
-// ================= PERMUTATION =================
-string initial_permutation(const string &input) {
-    int ip[64] = {
-        58,50,42,34,26,18,10,2,60,52,44,36,28,20,12,4,
-        62,54,46,38,30,22,14,6,64,56,48,40,32,24,16,8,
-        57,49,41,33,25,17,9,1,59,51,43,35,27,19,11,3,
-        61,53,45,37,29,21,13,5,63,55,47,39,31,23,15,7
-    };
-    string out = "";
-    for (int i = 0; i < 64; i++)
-        out += input[ip[i] - 1];
-    return out;
+// Hàm giả lập DES (đơn giản hóa)
+string fake_des_encrypt(string block, string key) {
+    return xor_str(block, key.substr(0, block.size()));
 }
 
-string inverse_initial_permutation(const string &input) {
-    int ip_inv[64] = {
-        40,8,48,16,56,24,64,32,39,7,47,15,55,23,63,31,
-        38,6,46,14,54,22,62,30,37,5,45,13,53,21,61,29,
-        36,4,44,12,52,20,60,28,35,3,43,11,51,19,59,27,
-        34,2,42,10,50,18,58,26,33,1,41,9,49,17,57,25
-    };
-    string out = "";
-    for (int i = 0; i < 64; i++)
-        out += input[ip_inv[i] - 1];
-    return out;
+string fake_des_decrypt(string block, string key) {
+    return xor_str(block, key.substr(0, block.size()));
 }
 
-// ================= KEY GENERATOR =================
-class KeyGenerator {
-private:
-    string key;
-
-public:
-    KeyGenerator(string k) : key(k) {}
-
-    vector<string> generate() {
-        vector<string> roundKeys;
-
-        int pc1[56] = {
-            57,49,41,33,25,17,9,1,58,50,42,34,26,18,
-            10,2,59,51,43,35,27,19,11,3,60,52,44,36,
-            63,55,47,39,31,23,15,7,62,54,46,38,30,22,
-            14,6,61,53,45,37,29,21,13,5,28,20,12,4
-        };
-
-        int pc2[48] = {
-            14,17,11,24,1,5,3,28,15,6,21,10,
-            23,19,12,4,26,8,16,7,27,20,13,2,
-            41,52,31,37,47,55,30,40,51,45,33,48,
-            44,49,39,56,34,53,46,42,50,36,29,32
-        };
-
-        int shift[16] = {1,1,2,2,2,2,2,2,1,2,2,2,2,2,2,1};
-
-        string perm = "";
-        for (int i = 0; i < 56; i++)
-            perm += key[pc1[i] - 1];
-
-        string left = perm.substr(0, 28);
-        string right = perm.substr(28, 28);
-
-        for (int i = 0; i < 16; i++) {
-            rotate(left.begin(), left.begin() + shift[i], left.end());
-            rotate(right.begin(), right.begin() + shift[i], right.end());
-
-            string combined = left + right;
-            string roundKey = "";
-
-            for (int j = 0; j < 48; j++)
-                roundKey += combined[pc2[j] - 1];
-
-            roundKeys.push_back(roundKey);
-        }
-
-        return roundKeys;
+// Padding zero
+vector<string> split_blocks(string s) {
+    vector<string> blocks;
+    for (int i = 0; i < s.size(); i += 64) {
+        string block = s.substr(i, 64);
+        while (block.size() < 64) block += '0';
+        blocks.push_back(block);
     }
-};
+    return blocks;
+}
 
-// ================= DES =================
-class DES {
-private:
-    vector<string> keys;
-
-public:
-    DES(vector<string> k) : keys(k) {}
-
-    string process(string input, bool decrypt = false) {
-        vector<string> roundKeys = keys;
-        if (decrypt) reverse(roundKeys.begin(), roundKeys.end());
-
-        string perm = initial_permutation(input);
-        string left = perm.substr(0, 32);
-        string right = perm.substr(32, 32);
-
-        int expansion_table[48] = {
-            32,1,2,3,4,5,4,5,6,7,8,9,8,9,10,11,
-            12,13,12,13,14,15,16,17,16,17,18,19,
-            20,21,20,21,22,23,24,25,24,25,26,27,
-            28,29,28,29,30,31,32,1
-        };
-
-        int permutation_tab[32] = {
-            16,7,20,21,29,12,28,17,
-            1,15,23,26,5,18,31,10,
-            2,8,24,14,32,27,3,9,
-            19,13,30,6,22,11,4,25
-        };
-
-        int sbox[8][4][16] = {
-            {{14,4,13,1,2,15,11,8,3,10,6,12,5,9,0,7},
-             {0,15,7,4,14,2,13,1,10,6,12,11,9,5,3,8},
-             {4,1,14,8,13,6,2,11,15,12,9,7,3,10,5,0},
-             {15,12,8,2,4,9,1,7,5,11,3,14,10,0,6,13}},
-
-            {{15,1,8,14,6,11,3,4,9,7,2,13,12,0,5,10},
-             {3,13,4,7,15,2,8,14,12,0,1,10,6,9,11,5},
-             {0,14,7,11,10,4,13,1,5,8,12,6,9,3,2,15},
-             {13,8,10,1,3,15,4,2,11,6,7,12,0,5,14,9}},
-
-            {{10,0,9,14,6,3,15,5,1,13,12,7,11,4,2,8},
-             {13,7,0,9,3,4,6,10,2,8,5,14,12,11,15,1},
-             {13,6,4,9,8,15,3,0,11,1,2,12,5,10,14,7},
-             {1,10,13,0,6,9,8,7,4,15,14,3,11,5,2,12}},
-
-            {{7,13,14,3,0,6,9,10,1,2,8,5,11,12,4,15},
-             {13,8,11,5,6,15,0,3,4,7,2,12,1,10,14,9},
-             {10,6,9,0,12,11,7,13,15,1,3,14,5,2,8,4},
-             {3,15,0,6,10,1,13,8,9,4,5,11,12,7,2,14}},
-
-            {{2,12,4,1,7,10,11,6,8,5,3,15,13,0,14,9},
-             {14,11,2,12,4,7,13,1,5,0,15,10,3,9,8,6},
-             {4,2,1,11,10,13,7,8,15,9,12,5,6,3,0,14},
-             {11,8,12,7,1,14,2,13,6,15,0,9,10,4,5,3}},
-
-            {{12,1,10,15,9,2,6,8,0,13,3,4,14,7,5,11},
-             {10,15,4,2,7,12,9,5,6,1,13,14,0,11,3,8},
-             {9,14,15,5,2,8,12,3,7,0,4,10,1,13,11,6},
-             {4,3,2,12,9,5,15,10,11,14,1,7,6,0,8,13}},
-
-            {{4,11,2,14,15,0,8,13,3,12,9,7,5,10,6,1},
-             {13,0,11,7,4,9,1,10,14,3,5,12,2,15,8,6},
-             {1,4,11,13,12,3,7,14,10,15,6,8,0,5,9,2},
-             {6,11,13,8,1,4,10,7,9,5,0,15,14,2,3,12}},
-
-            {{13,2,8,4,6,15,11,1,10,9,3,14,5,0,12,7},
-             {1,15,13,8,10,3,7,4,12,5,6,11,0,14,9,2},
-             {7,11,4,1,9,12,14,2,0,6,10,13,15,3,5,8},
-             {2,1,14,7,4,10,8,13,15,12,9,0,3,5,6,11}}
-        };
-
-        for (int i = 0; i < 16; i++) {
-            string right_expanded = "";
-            for (int j = 0; j < 48; j++)
-                right_expanded += right[expansion_table[j] - 1];
-
-            string xored = xor_str(right_expanded, roundKeys[i]);
-
-            string sbox_out = "";
-            for (int j = 0; j < 8; j++) {
-                string block = xored.substr(j * 6, 6);
-                int row = (block[0] - '0') * 2 + (block[5] - '0');
-                int col = stoi(block.substr(1, 4), nullptr, 2);
-                sbox_out += bitset<4>(sbox[j][row][col]).to_string();
-            }
-
-            string perm2 = "";
-            for (int j = 0; j < 32; j++)
-                perm2 += sbox_out[permutation_tab[j] - 1];
-
-            string new_right = xor_str(left, perm2);
-            left = right;
-            right = new_right;
-        }
-
-        return inverse_initial_permutation(right + left);
-    }
-};
-
-// ================= MAIN =================
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     int mode;
-    string input, key;
-
     cin >> mode;
-    cin >> input;
-    cin >> key;
 
-    string result = "";
+    if (mode == 1) {
+        string plaintext, key;
+        cin >> plaintext >> key;
 
-    if (mode == 1 || mode == 2) {
-        KeyGenerator kg(key);
-        DES des(kg.generate());
+        vector<string> blocks = split_blocks(plaintext);
+        string result = "";
 
-        for (int i = 0; i < input.size(); i += 64) {
-            string block = input.substr(i, 64);
-            if (block.size() < 64)
-                block.append(64 - block.size(), '0');
-
-            result += des.process(block, mode == 2);
+        for (auto &b : blocks) {
+            result += fake_des_encrypt(b, key);
         }
+
+        cout << result << "\n";
     }
 
-    else if (mode == 3 || mode == 4) {
-        string k2, k3;
-        cin >> k2 >> k3;
+    else if (mode == 2) {
+        string ciphertext, key;
+        cin >> ciphertext >> key;
 
-        DES des1(KeyGenerator(key).generate());
-        DES des2(KeyGenerator(k2).generate());
-        DES des3(KeyGenerator(k3).generate());
+        vector<string> blocks = split_blocks(ciphertext);
+        string result = "";
 
-        for (int i = 0; i < input.size(); i += 64) {
-            string block = input.substr(i, 64);
-            if (block.size() < 64)
-                block.append(64 - block.size(), '0');
-
-            if (mode == 3) {
-                block = des1.process(block, false);
-                block = des2.process(block, true);
-                block = des3.process(block, false);
-            } else {
-                block = des3.process(block, true);
-                block = des2.process(block, false);
-                block = des1.process(block, true);
-            }
-
-            result += block;
+        for (auto &b : blocks) {
+            result += fake_des_decrypt(b, key);
         }
+
+        cout << result << "\n";
     }
 
-    cout << result << endl;
+    else if (mode == 3) {
+        string p, k1, k2, k3;
+        cin >> p >> k1 >> k2 >> k3;
+
+        string step1 = fake_des_encrypt(p, k1);
+        string step2 = fake_des_decrypt(step1, k2);
+        string step3 = fake_des_encrypt(step2, k3);
+
+        cout << step3 << "\n";
+    }
+
+    else if (mode == 4) {
+        string c, k1, k2, k3;
+        cin >> c >> k1 >> k2 >> k3;
+
+        string step1 = fake_des_decrypt(c, k3);
+        string step2 = fake_des_encrypt(step1, k2);
+        string step3 = fake_des_decrypt(step2, k1);
+
+        cout << step3 << "\n";
+    }
+
     return 0;
 }
